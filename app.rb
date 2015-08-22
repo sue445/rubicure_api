3ed2141106016819f2c4641c7d65690564e7d7c3
sue445 <sue445@sue445.net>
2015-08-22 13:43:41 +0900

    Impl /index

--- app.rb
+++ app.rb
@@ -4,11 +4,14 @@ require "slim"
 require "yaml"
 require "active_support/all"
 require "rollbar/middleware/sinatra"
+require "rubicure"
 
 class App < Sinatra::Base
   use Rollbar::Middleware::Sinatra
 
   get "/" do
+    @girls = Precure.all_stars
+    @series = Precure
     slim :index
   end
 end
